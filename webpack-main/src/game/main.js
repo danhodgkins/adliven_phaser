af2fa0a57e3834867b8ab90ab5c6451bc7c89920
danhodgkins <danhodgkins@gmail.com>
2025-07-10 11:12:25 +0100

    barebones three scene with animating cube

--- webpack-main/src/game/main.js
+++ webpack-main/src/game/main.js
@@ -15,11 +15,51 @@ const StartGame = (parent) => {
 
 export default StartGame;
 
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+import { BoxGeometry, Color, DirectionalLight, Mesh, MeshBasicMaterial, OrthographicCamera, PerspectiveCamera, Scene, Vector3, WebGLRenderer } from 'three';
+
 
 export class Game {
-    constructor(config) {
-        console.log("new game")
+    constructor({config, parent}) {
+
+        const scene = new Scene();
+        this.scene = scene;
+
+        const camera = new PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+
+        const renderer = new WebGLRenderer();
+        renderer.setSize( window.innerWidth, window.innerHeight );
+
+        const el = document.getElementById( parent );
+        el.appendChild( renderer.domElement );
+
+        
+        const controls = new OrbitControls( camera, renderer.domElement );
+        const loader = new GLTFLoader();
+
+        console.log("new game", controls, loader);
+
+        const geometry = new BoxGeometry( 1, 1, 1 );
+        const material = new MeshBasicMaterial( { color: 0x00ff00 } );
+        const cube = new Mesh( geometry, material );
+        scene.add( cube );
+        this.cube = cube;
+
+        camera.position.z = 5;
+
+        this.renderer = renderer;
+        this.camera = camera;
+        this.boundUpdate = this.update.bind(this);
+        renderer.setAnimationLoop( this.boundUpdate );
     }
+
+    update() {
+        this.cube.rotation.x += 0.01;
+        this.cube.rotation.y += 0.01;
+        this.renderer.render( this.scene, this.camera );
+    }
+
 
     destroy() {
        
